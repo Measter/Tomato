01641acf736f156b560c3a51ab7cb1c9733f4f93
mluto <m@luto.at>
2012-10-20 21:18:28 +0200

    Fixed tabs

--- Lettuce/Program.cs
+++ Lettuce/Program.cs
@@ -160,7 +160,7 @@ namespace Lettuce
 
             debugger = new Debugger(ref CPU);
             debugger.StartPosition = FormStartPosition.Manual;
-			if(RuntimeInfo.IsMacOSX)
+            if(RuntimeInfo.IsMacOSX)
                 debugger.Location = new Point(0, 22);
             else
                 debugger.Location = new Point(0, 0);
